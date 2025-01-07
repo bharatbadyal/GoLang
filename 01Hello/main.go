ab1a296eb5253683f421d14bafc87e16025c8244
Bharat <myprof219@gmail.com>
2025-01-07 21:39:36 +0530

    Practiced variables in go| declaration, initilization with type, and both in go using := syntax

--- 01Hello/main.go
+++ 01Hello/main.go
@@ -3,5 +3,20 @@ package main
 import "fmt"
 
 func main(){
-	fmt.Println("Hello")
+	fmt.Println("Hello");
+	var a = "String";
+	fmt.Println(a);
+
+	var b,c int = 1,2;
+	fmt.Println(b,c);
+
+	var d bool = true;
+	fmt.Println(d);
+
+	var e bool;
+	fmt.Println(e);
+
+	f := "Apple";
+	fmt.Println(f)
+
 }
