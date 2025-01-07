a0dcfcde3889758332b51cff2d1761410379ac8e
Bharat <myprof219@gmail.com>
2025-01-07 21:57:15 +0530

    Practised Constant

--- 01Hello/main.go
+++ 01Hello/main.go
@@ -1,22 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
+const s string ="constant";
 func main(){
-	fmt.Println("Hello");
-	var a = "String";
-	fmt.Println(a);
+	// fmt.Println("Hello");
+	// var a = "String";
+	// fmt.Println(a);
 
-	var b,c int = 1,2;
-	fmt.Println(b,c);
+	// var b,c int = 1,2;
+	// fmt.Println(b,c);
 
-	var d bool = true;
+	// var d bool = true;
+	// fmt.Println(d);
+
+	// var e bool;
+	// fmt.Println(e);
+
+	// f := "Apple";
+	// fmt.Println(f)
+
+	//   Constant : Go supports constant of character, string, boolean, and numeric values
+
+	// const declares a constant value. and it can appear anywhere a var statement can.
+
+	fmt.Println(s);
+	const n = 500000;
+	const d = 3e20 /n;
 	fmt.Println(d);
+	fmt.Println(int64(d));
 
-	var e bool;
-	fmt.Println(e);
-
-	f := "Apple";
-	fmt.Println(f)
+	fmt.Println(math.Sin(n))
 
 }
