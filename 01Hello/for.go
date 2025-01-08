798933f21d14c3affd5e5a128364b6f1a5cf1c1b
Bharat <myprof219@gmail.com>
2025-01-08 21:56:53 +0530

    Added for.go file to practice the learned concepts

--- 01Hello/for.go
+++ 01Hello/for.go
@@ -2,7 +2,17 @@ package main
 
 import "fmt"
 
-func ma(){
-	fmt.Print("Hello From For");
+func main(){
+	fmt.Println("Hello From For");
+	
+	for i:=0; i<10 ; i++{
+		fmt.Println("i := ",i)
+	}
+
+	for n:= range 10{
+		if(n%2 ==0){
+			fmt.Println("Even no. := ",n);
+		}
+	}
 }
 
