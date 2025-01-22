e134c04f2f3f3ab07be0dff44127eb3b706f924b
Bharat <myprof219@gmail.com>
2025-01-22 17:27:11 +0530

    Built in data type

--- 01Hello/closures.go
+++ 01Hello/closures.go
@@ -19,24 +19,57 @@ import "fmt"
 
 // }
 
-func fact(n int) int{
-	if n==0{
-		return 1
-	}
-	return n*fact(n-1)
+// func fact(n int) int{
+// 	if n==0{
+// 		return 1
+// 	}
+// 	return n*fact(n-1)
+// }
+
+// func main(){
+// 	fmt.Println(fact(7))
+
+// 	var fib func(n int)int
+
+// 	fib = func(n int)int{
+// 		if n<2{
+// 			return n
+// 		}
+// 		return fib(n-1) + fib(n-2)
+// 	}
+
+	// fmt.Println(fib(7))
+
+	package main
+
+import "fmt"
+
+func main() {
+
+    nums := []int{2, 3, 4}
+    sum := 0
+    for _, num := range nums {
+        sum += num
+    }
+    fmt.Println("sum:", sum)
+
+    for i, num := range nums {
+        if num == 3 {
+            fmt.Println("index:", i)
+        }
+    }
+
+    kvs := map[string]string{"a": "apple", "b": "banana"}
+    for k, v := range kvs {
+        fmt.Printf("%s -> %s\n", k, v)
+    }
+
+    for k := range kvs {
+        fmt.Println("key:", k)
+    }
+
+    for i, c := range "go" {
+        fmt.Println(i, c)
+    }
 }
-
-func main(){
-	fmt.Println(fact(7))
-
-	var fib func(n int)int
-
-	fib = func(n int)int{
-		if n<2{
-			return n
-		}
-		return fib(n-1) + fib(n-2)
-	}
-
-	fmt.Println(fib(7))
 }
